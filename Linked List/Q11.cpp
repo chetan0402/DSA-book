aea9c5bf99014c287fbf7529e27c94a8edd2d14b
Chetan0402 <chetansharma67g@gmail.com>
2025-01-29 18:26:12 +0530

    resolve the question

--- Linked List/Q11.cpp
+++ Linked List/Q11.cpp
@@ -1,40 +1,36 @@
 #include<iostream>
 #include "SLL.cpp"
+#include<stack>
 using namespace std;
 
-Node*getnthElement(Node*head,int k){
-    while(k--){
-        head=head->next;
-    }
-    return head;
-}
-
-bool hasKElements(Node*head,int k){
-    k--;
-    while(k--){
-        if(head==NULL) return false;
-        head=head->next;
-    }
-    return true;
-}
-
 Node*ReverseBlockOfKnodes(Node*head,int k){
-    if(k==0 || k==1) return head;
+    int k_dup=k;
     Node*cur=head;
-    if(hasKElements(cur,k)){
-        Node*newHead=getnthElement(cur,k-1);
-        Node*prevNode=ReverseBlockOfKnodes(newHead->next,k);
-        Node*nextNode=cur->next;
-        for(int i=0;i<k;i++){
-            cur->next=prevNode;
-            prevNode=cur;
-            cur=nextNode;
-            if(nextNode!=NULL) nextNode=nextNode->next;
-        }
-        return newHead;
-    }else{
-        return head;
+    stack<Node*> recur;
+    // Loop for k times
+    // Check if the list has enough items to reverse the segment
+    // If it doesn't then return the original head since the segment didn't change
+    // Else keep pushing everything onto a stack so we can reverse later.
+    while(k_dup--){
+        if(cur==nullptr) return head;
+        recur.push(cur);
+        cur=cur->next;
     }
+    // Pre-store the new head so we can return it at the end.
+    auto new_head=recur.top();
+
+    // For all non-ending nodes, reverse the pointer to the one below it in the stack i.e behind in the list
+    while(recur.size()!=1){
+        auto forward=recur.top();
+        recur.pop();
+        auto back=recur.top();
+        forward->next=back;
+    }
+
+    // For the ending node, recursive k-reverse the next segment and concate it.
+    recur.top()->next=ReverseBlockOfKnodes(cur,k);
+    // Return the new head of the reversed segment.
+    return new_head;
 }
 
 int main(){
